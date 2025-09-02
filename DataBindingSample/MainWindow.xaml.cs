b0b2ab2a1257d56aac4cdd43638a3c4f3695ef3e
박상호 <tkdgh816@outlook.com>
2025-09-02 23:14:31 +0900

    앱 아이콘 추가 및 적용

--- DataBindingSample/MainWindow.xaml.cs
+++ DataBindingSample/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
+using System.IO;
+
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+
+using Windows.ApplicationModel;
 
 namespace DataBindingSample
 {
@@ -12,6 +16,7 @@ namespace DataBindingSample
       presenter.PreferredMinimumWidth = 800;
       presenter.PreferredMinimumHeight = 1000;
       AppWindow.Resize(new(800, 1000));
+      AppWindow.SetIcon(Path.Combine(Package.Current.InstalledLocation.Path, "Assets/Icon/AppIcon_128.ico"));
     }
   }
 }
